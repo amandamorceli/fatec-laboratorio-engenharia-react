1c220c5aa07b6eb8052f67d0569474bc98c9dd41
amandamorceli <138326308+amandamorceli@users.noreply.github.com>
2025-06-02 19:56:05 -0300

    Alguns exercícios da semana 4

--- src/components/Semana04/Semana04.js
+++ src/components/Semana04/Semana04.js
@@ -3,6 +3,10 @@ import Toolbar from "./Toolbar";
 import Gallery from "./Gallery";
 import Form from "./Form";
 import MovingDot from "./MovingDot";
+import Form2 from "./Form2";
+import Form3 from "./Form3";
+import List from "./List";
+import ShapeEditor from "./ShapeEditor";
 
 function Semana04() {
   return (
@@ -20,6 +24,16 @@ function Semana04() {
       <div>
         <MovingDot />
       </div>
+      <div>
+        <Form2/>
+      </div>
+      <div>
+        <Form3/>
+      </div>
+      <div>
+        <List/>
+      </div>
+      <div><ShapeEditor/></div>
     </div>
   );
 }
